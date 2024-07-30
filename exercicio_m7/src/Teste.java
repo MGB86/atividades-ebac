12f9d2f066bec054e002f192489459d6c5469730
Miguel <miguelbartz.profissional@gmail.com>
2024-07-30 20:50:45 -0300

    first commit

---
+++ exercicio_m7/src/Teste.java
@@ -0,0 +1,22 @@
+public class Teste {
+
+    public static void main(String[] args){
+        System.out.println("Olá Miguel");
+        SmartPhone smartPhone = new SmartPhone();
+        smartPhone.setPreco(1500);
+        smartPhone.setMarca("Samsung");
+        smartPhone.setModelo("A15");
+        System.out.println(smartPhone.getPreco());
+        System.out.println(smartPhone.getMarca());
+        System.out.println(smartPhone.getModelo());
+        /**
+         * @author Miguel
+         *
+         * @since 30/07/2024
+         * @version 1.0
+         */
+    }
+}
+
+
+
