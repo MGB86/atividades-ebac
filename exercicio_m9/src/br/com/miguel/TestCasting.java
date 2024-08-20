83fa53a1ad86808043bb071958b80608fd5051b9
Miguel <miguelbartz.profissional@gmail.com>
2024-08-19 21:25:40 -0300

    tentativa 2

---
+++ exercicio_m9/src/br/com/miguel/TestCasting.java
@@ -0,0 +1,14 @@
+package br.com.miguel;
+
+public class TestCasting {
+
+    public static void main(String[] args){
+
+        int i = 10;
+        System.out.println(i);
+
+        Integer in = i;
+        System.out.println(in);
+
+    }
+}
